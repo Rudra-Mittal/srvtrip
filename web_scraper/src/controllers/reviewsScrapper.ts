63b39617c1379b0ee35e610d948b2d84bc5bc8ab
Shivam-knight-owl <shivam050205@gmail.com>
2025-03-09 15:48:09 +0530

    minor changes

--- web_scraper/src/controllers/reviewsScrapper.ts
+++ web_scraper/src/controllers/reviewsScrapper.ts
@@ -1,4 +1,4 @@
-import {chromium } from 'playwright';
+import {chromium } from 'playwright'                                    ;
 
 export async function scrapeGoogleMapsReviews(placeName:string, maxScrolls:number) {
     const browser = await chromium.launch({ headless: true });  // Set headless: true to run without UI
