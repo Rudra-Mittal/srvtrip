f965aa8227f17b6e2038c1ac9220255d8a79ee1c
Rudra Mittal <rudramittal24@gmail.com>
2025-03-14 00:54:21 +0530

    created : backend server

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { placeInfo } from './controllers/places';
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.post('/api/places',async (req,res)=>{
+    const {placename} = req.body;
+   const data= await  placeInfo(placename)
+    res.send(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
