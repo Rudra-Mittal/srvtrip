885a42b063bbac8b467a15a9d0d822d041540763
Rudra Mittal <rudramittal24@gmail.com>
2025-04-27 12:14:21 +0530

    removed:dist

--- load_balancer/dist/config/scraperUrls.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.scraperServers = void 0;
-exports.scraperServers = [
-    { id: 1, url: "http://localhost:3000/" },
-    { id: 2, url: "http://localhost:3001/" },
-    { id: 3, url: "http://localhost:3002/" },
-    { id: 4, url: "http://localhost:3003/" },
-];
